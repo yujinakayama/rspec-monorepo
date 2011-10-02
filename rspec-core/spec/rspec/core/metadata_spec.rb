rspec/rspec-metagem

commit 0704cbb2fb59649658991009a227848af9d7f1c5

lazy eval description, full_description, and describes
- doesn't have much impact yet since the autogen descriptions still
  check for the presence of description, therefore it gets eval'd, but
  that is fixable!

lib/rspec/core/metadata.rb
@@ -2,7 +2,7 @@ module RSpec
   module Core
     class Metadata < Hash
 
-      module LocationKeys
+      module MetadataHash
         def [](key)
           return super if has_key?(key)
           case key
@@ -13,6 +13,14 @@ module RSpec
             store(:file_path, file_path)
             store(:line_number, line_number)
             self[key]
+          when :execution_result
+            store(:execution_result, {})
+          when :describes
+            store(:describes, described_class_from(*self[:description_args]))
+          when :description
+            store(:description, description_from(*self[:description_args]))
+          when :full_description
+            store(:full_description, full_description)
           else
             super
           end
@@ -30,17 +38,48 @@ module RSpec
         def first_caller_from_outside_rspec
           self[:caller].detect {|l| l !~ /\/lib\/rspec\/core/}
         end
+
+        def description_from(*args)
+          args.inject("") do |result, a|
+            a = a.to_s.strip
+            if result == ""
+              a
+            elsif a =~ /^(#|::|\.)/
+              "#{result}#{a}"
+            else
+              "#{result} #{a}"
+            end
+          end
+        end
+
+        def full_description
+          x = self
+          all_args = [self[:description_args]].compact
+          while x.has_key?(:example_group)
+            x = x[:example_group]
+            all_args.unshift x[:description_args]
+          end
+          description_from(*all_args.flatten)
+        end
+
+        def described_class_from(*args)
+          x = self
+          while x.has_key?(:example_group)
+            x = x[:example_group]
+          end
+          y = x[:description_args].first 
+          String === y || Symbol === y ? nil :y
+        end
       end
 
       attr_reader :parent_group_metadata
 
       def initialize(parent_group_metadata=nil)
         if parent_group_metadata
-          @parent_group_metadata = parent_group_metadata
-          update(@parent_group_metadata)
-          store(:example_group, {:example_group => @parent_group_metadata[:example_group]}.extend(LocationKeys))
+          update(parent_group_metadata)
+          store(:example_group, {:example_group => parent_group_metadata[:example_group]}.extend(MetadataHash))
         else
-          store(:example_group, {}.extend(LocationKeys))
+          store(:example_group, {}.extend(MetadataHash))
         end
 
         yield self if block_given?
@@ -62,9 +101,6 @@ module RSpec
 
         self[:example_group].store(:description_args, args)
         self[:example_group].store(:caller, user_metadata.delete(:caller) || caller)
-        self[:example_group].store(:describes, described_class_from(*args))
-        self[:example_group].store(:description, description_from(*args))
-        self[:example_group].store(:full_description, full_description_from(*args))
         self[:example_group].store(:block, user_metadata.delete(:example_group_block))
 
         update(user_metadata)
@@ -93,21 +129,11 @@ EOM
       end
 
       def for_example(description, user_metadata)
-        dup.extend(LocationKeys).configure_for_example(description, user_metadata)
-      end
-
-      def [](key)
-        return super if has_key?(key)
-        case key
-        when :execution_result
-          store(:execution_result, {})
-        when :full_description
-          store(:full_description, full_description_from(self[:example_group][:full_description], self[:description]))
-        end
+        dup.extend(MetadataHash).configure_for_example(description, user_metadata)
       end
 
       def configure_for_example(description, user_metadata)
-        store(:description, description.to_s)
+        store(:description_args, [description])
         store(:caller, user_metadata.delete(:caller) || caller)
         update(user_metadata)
       end
@@ -175,29 +201,6 @@ EOM
         RSpec.world
       end
 
-      def description_from(*args)
-        args.inject("") do |result, a|
-          a = a.to_s.strip
-          if result == ""
-            a
-          elsif a =~ /^(#|::|\.)/
-            "#{result}#{a}"
-          else
-            "#{result} #{a}"
-          end
-        end
-      end
-
-      def full_description_from(*args)
-        parent_group_metadata && description_from(parent_group_metadata[:example_group][:full_description], *args) ||
-          description_from(*args)
-      end
-
-      def described_class_from(*args)
-        parent_group_metadata && parent_group_metadata[:example_group][:describes] || begin
-          args.first unless String === args.first || Symbol === args.first
-        end
-      end
     end
   end
 end

yujinakayama/rspec-monorepo

commit 481c11892eadedb9397b0ba4812593ded3dba6f4

[core] Ensure support for overriding described class.
Fixes bug introduced by 0704cbb2fb59649658991009a227848af9d7f1c5 and
exposed in rspec-rails.

Also refactored some names in Metadata.

---
This commit was imported from https://github.com/rspec/rspec-core/commit/50df862212da4c08fce859929041c7c0951b8fdf.

rspec-core/lib/rspec/core/metadata.rb
@@ -16,11 +16,11 @@ module RSpec
           when :execution_result
             store(:execution_result, {})
           when :describes
-            store(:describes, described_class_from(*self[:description_args]))
-          when :description
-            store(:description, description_from(*self[:description_args]))
+            store(:describes, described_class_for(self))
           when :full_description
-            store(:full_description, full_description)
+            store(:full_description, full_description_for(self))
+          when :description
+            store(:description, build_description_from(*self[:description_args]))
           else
             super
           end
@@ -39,40 +39,30 @@ module RSpec
           self[:caller].detect {|l| l !~ /\/lib\/rspec\/core/}
         end
 
-        def description_from(*args)
-          args.inject("") do |result, a|
-            a = a.to_s.strip
-            if result == ""
-              a
-            elsif a =~ /^(#|::|\.)/
-              "#{result}#{a}"
-            else
-              "#{result} #{a}"
-            end
+        def described_class_for(m)
+          while m.has_key?(:example_group)
+            return m[:example_group][:describes] if m[:example_group].has_key?(:describes)
+            m = m[:example_group]
           end
+          candidate = m[:description_args].first
+          String === candidate || Symbol === candidate ? nil : candidate
         end
 
-        def full_description
-          x = self
-          all_args = [self[:description_args]].compact
-          while x.has_key?(:example_group)
-            x = x[:example_group]
-            all_args.unshift x[:description_args]
+        def full_description_for(m)
+          parts = [m[:description]]
+          while m.has_key?(:example_group)
+            m = m[:example_group]
+            parts.unshift m[:full_description]
           end
-          description_from(*all_args.flatten)
+          build_description_from(*parts.flatten)
         end
 
-        def described_class_from(*args)
-          x = self
-          while x.has_key?(:example_group)
-            x = x[:example_group]
-          end
-          y = x[:description_args].first 
-          String === y || Symbol === y ? nil :y
+        def build_description_from(*parts)
+          parts.map {|p| p.to_s}.reduce do |desc, p|
+            p =~ /^(#|::|\.)/ ? "#{desc}#{p}" : "#{desc} #{p}"
+          end || ""
         end
       end
-
-      attr_reader :parent_group_metadata
 
       def initialize(parent_group_metadata=nil)
         if parent_group_metadata

rspec-core/spec/rspec/core/metadata_spec.rb
@@ -211,8 +211,8 @@ module RSpec
           end
         end
 
-        context "with describes from a superclass metadata" do
-          it "returns the superclass' described class" do
+        context "with describes from a parent group metadata" do
+          it "returns the parent group's described class" do
             sm = Metadata.new
             sm.process(String)
 
@@ -222,13 +222,37 @@ module RSpec
             m = m.for_example("example", {})
             m[:example_group][:describes].should be(String)
           end
+
+          it "can override a parent group's described class" do
+            parent = Metadata.new
+            parent.process(String)
+
+            child = Metadata.new(parent)
+            child.process(Hash)
+            child[:example_group][:describes] = Hash
+
+            grandchild = Metadata.new(child)
+            grandchild.process(Array)
+
+            grandchild.for_example("example", {}).tap do |e|
+              e[:example_group][:describes].should be(Hash)
+            end
+
+            child.for_example("example", {}).tap do |e|
+              e[:example_group][:describes].should be(Hash)
+            end
+
+            parent.for_example("example", {}).tap do |e|
+              e[:example_group][:describes].should be(String)
+            end
+          end
         end
       end
 
       describe ":description" do
         it "just has the example description" do
           m = Metadata.new
-          m.process('group')
+          m.process("group")
 
           m = m.for_example("example", {})
           m[:description].should eq("example")
@@ -237,7 +261,7 @@ module RSpec
         context "with a string" do
           it "provides the submitted description" do
             m = Metadata.new
-            m.process('group')
+            m.process("group")
 
             m[:example_group][:description].should eq("group")
           end
@@ -246,7 +270,7 @@ module RSpec
         context "with a non-string" do
           it "provides the submitted description" do
             m = Metadata.new
-            m.process('group')
+            m.process("group")
 
             m[:example_group][:description].should eq("group")
           end
@@ -258,6 +282,15 @@ module RSpec
             m.process(Object, 'group')
 
             m[:example_group][:description].should eq("Object group")
+          end
+        end
+
+        context "with empty args" do
+          it "returns empty string for [:example_group][:description]" do
+            m = Metadata.new
+            m.process()
+
+            m[:example_group][:description].should eq("")
           end
         end
       end